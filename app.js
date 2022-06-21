4d429d0fdd2e9b0e6e3f8972712b1a88061850b6
Peter Domokos <petedomokos@gmail.com>
2022-06-21 21:38:05 +0100

    changed words to ttest pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req, res){
-    res.send("IT WORKS")
+    res.send("IT STILL WORKS")
 })
 
 app.listen(process.env.PORT || 5000)
