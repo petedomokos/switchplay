89f9301b738a2117b95d47fbfeced28b812eaedd
Peter Domokos <petedomokos@gmail.com>
2023-10-28 01:25:15 +0200

    added 16 england player names and photos to assets

---
+++ client/data/englandPlayers.js
@@ -0,0 +1,19 @@
+const players = [
+    { nr:0, firstName:"Ted", surname:"Curd", pos:"GK" },
+    { nr:1, firstName:"Zach", surname:"Abbot", pos:"DEF" },
+    { nr:2, firstName:"Josh", surname:"Acheampong", pos:"DEF" },
+    { nr:3, firstName:"Joe", surname:"Johnson", pos:"DEF" },
+    { nr:4, firstName:"Jayden", surname:"Meghoma", pos:"DEF" },
+    { nr:5, firstName:"Samuel", surname:"Amo-Ameyaw", pos:"MID" },
+    { nr:6, firstName:"Michael", surname:"Golding", pos:"MID" },
+    { nr:7, firstName:"Finley", surname:"McAllister", pos:"MID" },
+    { nr:8, firstName:"Chris", surname:"Rigg", pos:"MID" },
+    { nr:9, firstName:"Jayden", surname:"Danns", pos:"F" },
+    { nr:10, firstName:"Jimmy-Jay", surname:"Morgan", pos:"F" },
+    { nr:11, firstName:"Zak", surname:"Lovelace", pos:"F" },
+    { nr:12, firstName:"Joel", surname:"NDala", pos:"F" },
+    { nr:13, firstName:"Tom", surname:"Watson", pos:"F" },
+    { nr:14, firstName:"Justin", surname:"Oboavwoduo", pos:"F" },
+    { nr:15, firstName:"Archie", surname:"Stevens", pos:"F" },
+    { nr:16, firstName:"Tom", surname:"King", pos:"F" }
+]
