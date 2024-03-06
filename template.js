dd1dbfd2bb957edd594efc5418a8e63dcd3ccd2b
Peter Domokos <petedomokos@gmail.com>
2024-03-06 21:40:54 +0100

    removed error quotes in content secirty policy url

--- template.js
+++ template.js
@@ -2,7 +2,7 @@ export default () => {
   return `<!doctype html>
     <html lang="en">
       <head>
-        <meta http-equiv="Content-Security-Policy" content="font-src 'self' 'unsafe-inline' https://fonts.gstatic.com data:; img-src 'self' data:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com data:; script-src 'unsafe-eval' 'unsafe-inline' http://localhost:3000/dist/bundle.js 'https://switchplay-app.herokuapp.com/dist/bundle.js' data:; default-src 'self' localhost:*">
+        <meta http-equiv="Content-Security-Policy" content="font-src 'self' 'unsafe-inline' https://fonts.gstatic.com data:; img-src 'self' data:; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com data:; script-src 'unsafe-eval' 'unsafe-inline' http://localhost:3000/dist/bundle.js https://switchplay-app.herokuapp.com/dist/bundle.js data:; default-src 'self' localhost:*">
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" charset="utf-8">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="theme-color" content="#DBEFF0" />
